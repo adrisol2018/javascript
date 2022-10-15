3fa8f6e6160ef7059c509dfa37bdf07dd46dab47
Adriana Solorzano <adriana198930@gmail.com>
2022-10-15 16:23:33 -0300

    first commit

--- js/documento.js
+++ js/documento.js
@@ -1,46 +1,108 @@
-//funciones
+const productos =  [
+    { nombre: "Vino Malbec Cordoba", precio: 2500},
+    { nombre: "Vino Cabernet Franc El Gran Enemigo", precio: 4650},
+    { nombre: "Vino Malbec Reserva Bag", precio: 3500},
+    { nombre: "Vino Mosquita Muerta", precio: 4500},
+    { nombre: "Vino SaintFelicien Malbec", precio: 2500},
+    { nombre: "Vino Cabernet Rutini ", precio: 3500},
+    { nombre: "Vino Blanco Gata Flora Torrontes", precio: 1500},
+    { nombre: "Vino Vistades Gradum Carmenere", precio: 2800},
+    { nombre: "Vino Cabernet Franc Castizo", precio: 1800},
+    { nombre: "Vino Cabernet Franc Ernesto Catena", precio: 2500},
+    { nombre: "Vino Cabernet Sauvignon El Esteco", precio: 2500},
+    { nombre: "Vino RedBlend Alma Negra", precio: 4500},
+    { nombre: "Vino RedBlend Andeluna Elevación", precio: 2500},
+    { nombre: "Vino RedBlend Cocodrilo", precio: 2500},
 
-function saludar (usuario){
-    console.log(" Bienvenido", usuario)
+];
+
+let carrito = []
+
+let seleccion = prompt("¿Desea comprar un vino si o no? ")
+
+while (seleccion != "si" && seleccion != "no" ) {
+    alert("por favor ingresa si o no")
+    seleccion = prompt ("¿Desea comprar un vino si o no?")
 }
 
-function login(){
-    let usuario = prompt ("Ingresa tu nombre de usuario:")
-        if (usuario.trim() !== "" && usuario.length > 2){
-            saludar (usuario)
-            } else {
-                console.warn8 ("No se reconoce el dato ingresado.")
-            }
+if (seleccion == "si") {
+    alert("a continuación nuestra lista de productos")
+   let todoslosVinos= productos.map((producto)=>producto.nombre+" "+producto.precio +"$");
+   alert(todoslosVinos.join("-"))    
+} else if(seleccion=="no"){
+    alert("Gracias por su visita, nos vemos pronto")
 }
 
+while (seleccion != "no") {
+    let producto = prompt("agrega un producto a tu carrito")
+    let precio = 0
 
-function verProductos(){
-    let productos = "Produstos disponibles: \n" +
-                    "1) Cabernet \n" +
-                    "2) Malbet \n" +
-                    "3) Red"
+    if (producto == "Vino Malbec Cordoba"|| producto == "Vino Cabernet Franc El Gran Enemigo"||producto == "Vino Malbec Reserva Bag"|| producto == "Vino Mosquita Muerta"|| producto == "Vino SaintFelicien Malbec"|| producto == "Vino Cabernet Rutini" || producto == "Vino Blanco Gata Flora Torrontes"|| producto == "Vino Vistades Gradum Carmenere"|| producto == "Vino Cabernet Franc Castizo "|| producto == "Vino Cabernet Franc Ernesto Catena"||producto == "Vino Cabernet Sauvignon El Esteco"|| producto == "Vino RedBlend Alma Negra"|| producto == "Vino RedBlend Andeluna Elevación"|| producto == "Vino RedBlend Cocodrilo" ) {
+        switch (producto) {
+            case "Vino Malbec Cordoba":
+                precio = 2500;
+                break;
+            case "Vino Cabernet Franc El Gran Enemigo":
+                precio = 4650;
+                break;
+            case "Vino Malbec Reserva Bag":
+                precio = 3500;
+                break;
+            case "Vino Mosquita Muerta":
+                precio = 4500;
+                break;
+            case "Vino SaintFelicien Malbec":
+                precio = 2500;
+                break;
+            case "Vino Cabernet Rutini ":
+                precio = 3500;
+                break;
+            case "Vino Blanco Gata Flora Torrontes":
+                precio = 1500;
+                break;
+            case "Vino Vistades Gradum Carmenere":
+                precio = 2800;
+                break;
+            case "Vino Cabernet Franc Castizo":
+                precio = 1800;
+                break;
+            case "Vino SaintFelicien Malbec":
+                precio = 2500;
+                break;
+            case "Vino Cabernet Franc Ernesto Catena":
+                precio = 2500;
+                break;
+            case "Vino Cabernet Sauvignon El Esteco":
+                precio = 2500;
+                break;
+            case "Vino RedBlend Alma Negra":
+                precio = 4500;
+                break;
+            case "Vino RedBlend Andeluna Elevación":
+                precio = 2500;
+                break;
+            case "Vino RedBlend Cocodrilo":
+                precio = 2500;
+                break;
+            default:
+                break;
+        }
 
-     let respuesta = prompt("Ingresa el nro. de producti para ver el precio : \n" + productos)
-     switch (parseInt(respuesta))    {
-        case 1: 
-            console.log("producto elegido 'Vino Cabernet'. Su precio es de $25000")
+        let unidades = parseInt(prompt("cuantas unidades qieres llevar"))
+     carrito.push({producto,unidades,precio})
+     console.log(carrito)
+    } else{
+        alert("no tenermos ese producto")
+    }
 
-          console.log("producto elegido 'Vino Malbet'. Su precio es de $20000")
-        case 2: 
-          console.log("producto elegido 'Vino Red'. Su precio es de $30000")
-
+    seleccion = prompt("¿desea seguir comprando?")
+    while (seleccion === "no") {
+        alert("gracias por la compra, vuelva pronto")
+        carrito.forEach((carritoFinal)=>{
+            console.log(`producto:${carritoFinal.producto}, unidades:${carritoFinal.unidades},
+            total a pagar por producto${carritoFinal.unidades*carritoFinal.precio} `)
+        })
         break;
-        case 3: 
-        console.log("producto elegido 'Vino Red'. Su precio es de $30000 ")
+    }
 
-        break;
-        default:
-            console.error ("No se conoce el producto ingresado")
-     } 
-     
-     let continuar = ("¿desea consultar el valor de otro producto? ")
-
-     if(continuar){
-        verProductos()
-     }
-     }          
+}
