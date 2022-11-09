7edcad4c989f4cf15f1a41a2b59001a88d3c7c9e
Adriana Solorzano <adriana198930@gmail.com>
2022-11-09 02:46:09 -0300

    dom y eventos

--- js/dom.js
+++ js/dom.js
@@ -1,85 +1,47 @@
-const container = document.querySelector("div.container");
+const containerPadre = document.querySelector("div.containerPadre");
 
-const retornoVino = (producto) => {
-  return `<div class="imag-1">
-                <img src="./img/PREMIUM-MALBEC.png" alt="" class="vino1" />
-                <p class="texto">Vino Malbec Cordoba</p>
-                <div class="card-name">Vino</div>
-                <p class="precio">$ 2500</p>
+const carrito = []
+
+const retornoVino= (producto) => {
+  return `<div class="card">
+                <div class="card-image">${producto.image} <img src="./img/PREMIUM-MALBEC.png" alt="" class="vino1" /></div>
+                <div class="card-name">${producto.nombre}</div>
+                <div class="card-price">${producto.precio}</div>
                 <div class="card-button">
-                    <button class="button button-outline button-add" id="idunico" title="Pulsa para agregar al carrito">+</button>
+                    <button class="button button-outline button-add" id=${producto.nombre} title="Pulsa para agregar al carrito">+</button>
                 </div>
-                </div>
-                <div class="imag-2">
-                <img src="./img/gran-enemigo.png" alt="" class="vino2" />
-                <p class="texto">Vino Cabernet Franc El Gran Enemigo</p>
-                <div class="card-name">Vino</div>
-                <p class="precio">$ 4650</p>
-                <div class="card-button">
-                    <button class="button button-outline button-add" id="idunico" title="Pulsa para agregar al carrito">+</button>
-                </div>
-                </div>
-                <div class="imag-3">
-                <img src="./img/reservabag.jpg" alt="" class="vino3" />
-                <p class="texto">Vino Malbec Reserva Bag</p>
-                <div class="card-name">Vino</div>
-                <p class="precio">$ 3500</p>
-                <div class="card-button">
-                    <button class="button button-outline button-add" id="idunico" title="Pulsa para agregar al carrito">+</button>
-                </div>
-                </div>
-            </div>`
+                </div>`
 }
 
-const container1 = document.querySelector("div.container1");
 
 const activarBotonAdd = () => {
-  const botonesAdd = document.querySelectorAll(".button.button-outline.button-add")
- 
+  const botonesAdd = document.querySelectorAll(".button.button-outline.button-add");
+
   botonesAdd.forEach(btn => {
-    btn.addEventListener("click",()=> {
-      agregarAlCarrito(btn.id)
+    btn.addEventListener("click", () => {
+      agregarAlCarrito(btn.id);
+    })
   })
-})
-  
-const retornoVino2 = (producto) => {
-  return `
-    <div class="imgPadre3">
-        <div class="img-7">
-          <img src="./img/torrentes.jpg" loading="lazy" alt="" class="vino7" />
-          <p class="texto">Vino Blanco Gata Flora Torrontes</p>
-           <div class="card-name">Vino</div>
-          <p class="precio">$ 1500</p>
-          <div class="card-button">
-            <button class="button button-outline button-add" id="idunico" title="Pulsa para agregar al carrito">+</button>
-          </div>
-        </div>
-        <div class="img-8">
-          <img src="./img/gradum-carmenere.png" loading="lazy" alt="" class="vino8" />
-          <p class="texto">Vino Vistades Gradum Carmenere</p>
-           <div class="card-name">Vino</div>
-          <p class="precio">$ 2800</p>
-          <div class="card-button">
-            <button class="button button-outline button-add" id="idunico" title="Pulsa para agregar al carrito">+</button>
-          </div>
-        </div>
-        <div class="img-9">
-          <img src="./img/castizo.jpg" loading="lazy" alt="" class="vino9" />
-          <p class="texto">Vino Cabernet Franc Castizo</p>
-           <div class="card-name">Vino</div>
-          <p class="precio">$ 1800</p>
-          <div class="card-button">
-            <button class="button button-outline button-add" id="idunico" title="Pulsa para agregar al carrito">+</button>
-          </div>
-        </div>
-      </div>`
+
+}
+ 
+const cargarMisProductos = ()=> {
+
+     containerPadre.innerHTML = ""
+    productos.forEach(producto => {
+        containerPadre.innerHTML += retornoVino(producto)
+    })
+    activarBotonAdd()
 }
 
-const cargarMisProductos = ()=> {
-    debugger;
-    container.HTML = ""
-    cargarMisProductos.forEach(producto => {
-        console.table(producto);
-        container.HTML += retornoVino(producto)
-    })
+cargarMisProductos()
+
+
+const agregarAlCarrito = (vino) => {
+  let resultado = productos.find(prod => prod.nombre  === vino)
+    if(resultado !== undefined){
+        carrito.push(resultado)
+        console.clear()
+        console.table(carrito)
+    }
 }
--- js/java.js
+++ js/java.js
@@ -1,114 +1,37 @@
 
 
 const productos =  [
-    { nombre: "Vino Malbec Cordoba", precio: 2500},
-    { nombre: "Vino Cabernet Franc El Gran Enemigo", precio: 4650},
-    { nombre: "Vino Malbec Reserva Bag", precio: 3500},
-    { nombre: "Vino Mosquita Muerta", precio: 4500},
-    { nombre: "Vino SaintFelicien Malbec", precio: 2500},
-    { nombre: "Vino Cabernet Rutini ", precio: 3500},
-    { nombre: "Vino Blanco Gata Flora Torrontes", precio: 1500},
-    { nombre: "Vino Vistades Gradum Carmenere", precio: 2800},
+    {  
+         nombre: "Vino Malbec Cordoba",
+          precio: 2500},
+    { 
+        nombre: "Vino Cabernet Franc El Gran Enemigo", 
+    precio: 4650},
+
+    { nombre: "Vino Malbec Reserva Bag",
+     precio: 3500},
+    { nombre: "Vino Mosquita Muerta",
+     precio: 4500},
+    { nombre: "Vino SaintFelicien Malbec", 
+    precio: 2500},
+    { nombre: "Vino Cabernet Rutini ", 
+    precio: 3500},
+    { nombre: "Vino Blanco Gata Flora Torrontes", 
+    precio: 1500},
+    { nombre: "Vino Vistades Gradum Carmenere",
+     precio: 2800},
     { nombre: "Vino Cabernet Franc Castizo", precio: 1800},
-    { nombre: "Vino Cabernet Franc Ernesto Catena", precio: 2500},
-    { nombre: "Vino Cabernet Sauvignon El Esteco", precio: 2500},
-    { nombre: "Vino RedBlend Alma Negra", precio: 4500},
-    { nombre: "Vino RedBlend Andeluna Elevación", precio: 2500},
-    { nombre: "Vino RedBlend Cocodrilo", precio: 2500},
-    { nombre: "Vino Rutini Blanc", precio: 2500},
+    { nombre: "Vino Cabernet Franc Ernesto Catena",
+     precio: 2500},
+    { nombre: "Vino Cabernet Sauvignon El Esteco",
+     precio: 2500},
+    { nombre: "Vino RedBlend Alma Negra",
+     precio: 4500},
+    { nombre: "Vino RedBlend Andeluna Elevación",
+     precio: 2500},
+    { nombre: "Vino RedBlend Cocodrilo",
+     precio: 2500},
+    { nombre: "Vino Rutini Blanc",
+     precio: 2500}
 
-];
-
-let carrito = []
-
-let seleccion = prompt("¿Desea comprar un vino si o no? ")
-
-while (seleccion != "si" && seleccion != "no" ) {
-    alert("por favor ingresa si o no")
-    seleccion = prompt ("¿Desea comprar un vino si o no?")
-}
-
-if (seleccion == "si") {
-    alert("a continuación nuestra lista de productos")
-   let todoslosVinos= productos.map((producto)=>producto.nombre+" "+producto.precio +"$");
-   alert(todoslosVinos.join("-"))    
-} else if(seleccion=="no"){
-    alert("Gracias por su visita, nos vemos pronto")
-}
-
-while (seleccion != "no") {
-    let producto = prompt("agrega un producto a tu carrito")
-    let precio = 0
-
-    if (producto == "Vino Malbec Cordoba"|| producto == "Vino Cabernet Franc El Gran Enemigo"||producto == "Vino Malbec Reserva Bag"|| producto == "Vino Mosquita Muerta"|| producto == "Vino SaintFelicien Malbec"|| producto == "Vino Cabernet Rutini" || producto == "Vino Blanco Gata Flora Torrontes"|| producto == "Vino Vistades Gradum Carmenere"|| producto == "Vino Cabernet Franc Castizo "|| producto == "Vino Cabernet Franc Ernesto Catena"||producto == "Vino Cabernet Sauvignon El Esteco"|| producto == "Vino RedBlend Alma Negra"|| producto == "Vino RedBlend Andeluna Elevación"|| producto == "Vino RedBlend Cocodrilo"  || "Vino Rutini Blanc") {
-        switch (producto) {
-            case "Vino Malbec Cordoba":
-                precio = 2500;
-                break;
-            case "Vino Cabernet Franc El Gran Enemigo":
-                precio = 4650;
-                break;
-            case "Vino Malbec Reserva Bag":
-                precio = 3500;
-                break;
-            case "Vino Mosquita Muerta":
-                precio = 4500;
-                break;
-            case "Vino SaintFelicien Malbec":
-                precio = 2500;
-                break;
-            case "Vino Cabernet Rutini ":
-                precio = 3500;
-                break;
-            case "Vino Blanco Gata Flora Torrontes":
-                precio = 1500;
-                break;
-            case "Vino Vistades Gradum Carmenere":
-                precio = 2800;
-                break;
-            case "Vino Cabernet Franc Castizo":
-                precio = 1800;
-                break;
-            case "Vino SaintFelicien Malbec":
-                precio = 2500;
-                break;
-            case "Vino Cabernet Franc Ernesto Catena":
-                precio = 2500;
-                break;
-            case "Vino Cabernet Sauvignon El Esteco":
-                precio = 2500;
-                break;
-            case "Vino RedBlend Alma Negra":
-                precio = 4500;
-                break;
-            case "Vino RedBlend Andeluna Elevación":
-                precio = 2500;
-                break;
-            case "Vino RedBlend Cocodrilo":
-                precio = 2500;
-                break;
-            case "Vino Rutini Blanc":
-                precio= 2500;
-            break;
-            default:
-                break;
-        }
-
-        let unidades = parseInt(prompt("cuantas unidades qieres llevar"))
-     carrito.push({producto,unidades,precio})
-     console.log(carrito)
-    } else{
-        alert("no tenermos ese producto")
-    }
-
-    seleccion = prompt("¿desea seguir comprando?")
-    while (seleccion === "no") {
-        alert("gracias por la compra, vuelva pronto")
-        carrito.forEach((carritoFinal)=>{
-            console.log(`producto:${carritoFinal.producto}, unidades:${carritoFinal.unidades},
-            total a pagar por producto${carritoFinal.unidades*carritoFinal.precio} `)
-        })
-        break;
-    }
-
-}
+]
